22b876e42131ee1bb1daffcf501be5006b04afac
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-04-10 12:31:13 -0300

    Revert "cambio menor carpeta raiz"
    This reverts commit b01297d6d1c3abae13874ffe9f952fa2a75e700e.

--- TpReact1/vite.config.ts
+++ TpReact1/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TpReact1/',
+  base: '/TP-React-Lab4/',
   plugins: [react()],
 })
