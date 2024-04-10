0e669667dd4d160c63168ad828d68704b0be3404
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-04-10 13:06:09 -0300

    .

--- TpReact1/vite.config.ts
+++ TpReact1/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'TP-React-Lab4',
+  base: '/TpReact1/',
   plugins: [react()],
 })
