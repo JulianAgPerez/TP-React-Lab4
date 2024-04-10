b01297d6d1c3abae13874ffe9f952fa2a75e700e
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-04-10 12:11:45 -0300

    cambio menor carpeta raiz

--- TpReact1/vite.config.ts
+++ TpReact1/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TP-React-Lab4/',
+  base: '/TP-React-Lab4/TpReact1/',
   plugins: [react()],
 })
