008ff14b3fa269ff641be93e32a6c714250708dc
JulianAgPerez <julian.perez@alumnos.frm.utn.edu.ar>
2024-05-31 13:07:54 -0300

    Agregado ocultacion de header en login

--- TpReact1/src/routes/Route.tsx
+++ TpReact1/src/routes/Route.tsx
@@ -1,10 +1,17 @@
+import { useLocation } from "react-router-dom";
 import { HeaderHome } from "../components/ui/Header/header";
 import { Outlet } from "react-router-dom";
 
 export const Route = () => {
+  const location = useLocation();
+
+  const hideHeaderPaths = ["/login"];
+
+  const shouldShowHeader = !hideHeaderPaths.includes(location.pathname);
+
   return (
     <div>
-      <HeaderHome />
+      {shouldShowHeader && <HeaderHome />}
       <Outlet />
     </div>
   );
